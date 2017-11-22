6c929760620ccfd07776716db2d423858301e360
Linh Dang <linh.danghong@gmail.com>
2017-11-22 22:01:07 +0000

    Step 2: Count number of login times

--- src/main/java/com/in28minutes/springboot/web/service/LoginService.java
+++ src/main/java/com/in28minutes/springboot/web/service/LoginService.java
@@ -1,10 +1,14 @@
 package com.in28minutes.springboot.web.service;
 
+import com.beeinstant.metrics.MetricsLogger;
+import com.beeinstant.metrics.MetricsManager;
 import org.springframework.stereotype.Component;
 
 @Component
 public class LoginService {
+    private static final MetricsLogger metricsLogger = MetricsManager.getMetricsLogger("service=LoginService");
     public boolean validateUser(String user, String password) {
+        metricsLogger.incCounter("userLoginCounter", 1);
         return user.equalsIgnoreCase("in28Minutes") && password.equals("dummy");
     }
 }
